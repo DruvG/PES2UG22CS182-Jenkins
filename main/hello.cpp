f2d12d5a29de4a788c50f35d710ea56bcfe8ef15
Dhruv <147785825+DruvG@users.noreply.github.com>
2025-03-14 16:24:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this PES2UG22CS182-Dhruv After making changes \n";
+  cout << "I have successfully built and run this PES2UG22CS182-Dhruv After changes \n";
   return 0;
 }
