a2c628fd9d0b4df565392857eefa6853a25de5f5
Dhruv <147785825+DruvG@users.noreply.github.com>
2025-03-18 15:55:14 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, PES2UG22CS182\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully finished lab 8 \n";
+  return 0;
+}
