7d9177ddc90b6782a174594232bacb0304fa31d5
Dhruv <147785825+DruvG@users.noreply.github.com>
2025-03-18 15:49:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this PES2UG22CS182-After commit \n";
+  cout << "I have successfully built and run this PES2UG22CS182-After commit changes \n";
   return 0;
 }
